423d520906c81321157e1dd59eca7332436aaa75
PetkaKotletka <podlesskiy2014@gmail.com>
2024-03-14 11:42:19 +0100

    lol

--- frontend/src/app/registered/modify/page.js
+++ frontend/src/app/registered/modify/page.js
@@ -1,115 +1,136 @@
 "use client"
 
-import React, { useState, useEffect } from 'react'
-import RegisteredNavbar from '@/components/RegisteredNavbar'
 import MultiSelect from '@/components/MultiSelect'
-import { useParams } from 'next/navigation'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import { BACKEND_URI } from '@/config/env'
 
-export default function ModifyUser() {
+export default function UpdateUser() {
     const router = useRouter()
-    const params = useParams()
-    const [userData, setUserData] = useState(null)
-
-    const [interests, setInterests] = useState([])
-    const [receiveOffers, setReceiveOffers] = useState(false)
+    const [nombre, setNombre] = useState('')
+    const [instrumento, setInstrumento] = useState([])
+    const [gustoMusical, setGustoMusical] = useState('')
+    const [bio, setBio] = useState('')
+    const [nickname, setNickname] = useState('')
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`/api/users/${params.userId}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                })
+        const fetchUserData = async () => {
+            const token = localStorage.getItem('token')
+            if (!token) {
+                router.push('/login')
+                return
+            }
 
-                const data = await response.json()
-                setUserData(data.data)
-            } catch (error) {
-                console.error("Error: ", error)
+            const response = await fetch(`${BACKEND_URI}/usuario/getUserData`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            if (response.ok) {
+                const userData = await response.json()
+                setNombre(userData.nombre)
+                setInstrumento(userData.instrumento)
+                setGustoMusical(userData.gusto_musical)
+                setBio(userData.bio)
+                setNickname(userData.nickname)
+            } else {
+                router.push('/login')
             }
         }
 
-        fetchData()
-    }, [params.userId])
-
-    if (!userData) {
-        return <div>Loading...</div>
-    }
+        fetchUserData()
+    }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const user = {
-            id: userData.id,
-            name: userData.name,
-            age: userData.age,
-            city: "Madrid",
-            interests: interests,
-            receiveOffers: receiveOffers,
-            email: userData.email,
-            password: userData.password,
+        const token = localStorage.getItem('token')
+        if (!token) {
+            router.push('/login')
+            return
         }
 
-        fetch('/api/users', {
-            method: 'PUT',
+        const updatedUser = {
+            nombre,
+            instrumento,
+            gusto_musical: gustoMusical,
+            bio,
+            nickname
+        }
+
+        fetch(`${BACKEND_URI}/usuario/updateUserData`, {
+            method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
             },
-            body: JSON.stringify(user),
+            body: JSON.stringify(updatedUser),
         })
             .then((res) => res.json())
             .then((data) => console.log(data))
 
-        router.push(`/registered/${params.userId}`)
+        router.push('/profile')
     }
 
-    const handleDeleteAccount = () => {
-        const isConfirmed = window.confirm('¿Estás seguro/a de que quieres eliminar tu cuenta?')
-
-        if (isConfirmed) {
-            fetch('/api/users', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({id: userData.id}),
-            })
-                .then((res) => res.json())
-                .then((data) => console.log(data))
-
-            router.push("/")
-        }
-    }
-
-    const interestsOptions = ['violin', 'guitarra', 'percusion', 'teclado']
+    var instrumentoOptions = ['violin', 'guitarra', 'percusion', 'teclado']
+    var nivelOptions = ['Principiante', 'Medio', 'Avanzado']
 
     return (
         <section>
-            <div>
-                <RegisteredNavbar userId={params.userId} userEmail={userData.email} />
-            </div>
-            <div className="h-screen bg-gray-100 flex justify-center">
-                <div className="py-6 px-8 h-96 mt-20 bg-white rounded shadow-xl">
+            <div className="min-h-screen bg-gray-100 flex justify-center">
+                <div className="py-6 px-8 mt-20 bg-white rounded shadow-xl">
                     <form onSubmit={handleSubmit}>
                         <div className="mb-6">
+                            <label htmlFor="nombre" className="block text-gray-800 font-bold">Nombre completo:</label>
+                            <input value={nombre} onChange={(e) => setNombre(e.target.value)} type="text" name="nombre" id="nombre" placeholder="John Doe" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
+                        </div>
+
+                        <div className="mb-6">
                             <MultiSelect
-                                formFieldName={"interests"}
-                                options={interestsOptions}
-                                onChange={(e) => setInterests(e)}
+                                formFieldName={"instrumento"}
+                                options={instrumentoOptions}
+                                value={instrumento.map(inst => inst.nombre)}
+                                onChange={(selectedOptions) => setInstrumento(selectedOptions.map(option => ({ nombre: option, niveles: [] })))}
                                 prompt="Seleccione uno o mas instrumentos" />
                         </div>
+
                         <div className="mb-6">
-                            <label htmlFor="offers" className="block text-gray-800 font-bold">¿Quieres recibir nuestras ofertas especiales?</label>
-                            <input onChange={(e) => setReceiveOffers(e.target.checked)} type="checkbox" name="offers" id="offers" />
+                            <label htmlFor="gustoMusical" className="block text-gray-800 font-bold">Gusto musical:</label>
+                            <input value={gustoMusical} onChange={(e) => setGustoMusical(e.target.value)} type="text" name="gustoMusical" id="gustoMusical" placeholder="Tu gusto musical" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
                         </div>
+
                         <div className="mb-6">
-                            <div className="flex justify-between">
-                                <button type="submit" className="py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold rounded">Actualizar</button>
-                                <button type="button" onClick={handleDeleteAccount} className="py-2 px-4 block mt-6 bg-red-500 text-white font-bold rounded">Borrar cuenta</button>
+                            <label htmlFor="bio" className="block text-gray-800 font-bold">Biografía:</label>
+                            <textarea value={bio} onChange={(e) => setBio(e.target.value)} name="bio" id="bio" rows="3" placeholder="Cuéntanos algo sobre ti..." className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600"></textarea>
+                        </div>
+
+                        {instrumento.map((inst, index) => (
+                            <div key={inst.nombre} className="mb-6">
+                                <MultiSelect
+                                    formFieldName={`nivel_${inst.nombre}`}
+                                    options={nivelOptions}
+                                    value={inst.niveles}
+                                    onChange={(selectedOptions) => {
+                                        const newInstrumento = instrumento.map((instrumento, idx) => {
+                                            if (index === idx) {
+                                                return { ...instrumento, nivel: selectedOptions }
+                                            }
+                                            return instrumento
+                                        })
+                                        setInstrumento(newInstrumento)
+                                    }}
+                                    prompt={`Seleccione el nivel para ${inst.nombre}`}
+                                />
                             </div>
+                        ))}
+
+                        <div className="mb-6">
+                            <label htmlFor="nickname" className="block text-gray-800 font-bold">Nickname:</label>
+                            <input value={nickname} onChange={(e) => setNickname(e.target.value)} type="text" name="nickname" id="nickname" placeholder="Tu apodo" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
                         </div>
+
+                        <button type="submit" className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">Actualizar datos</button>
                     </form>
                 </div>
             </div>
