20d166309e4bf3c5605d097ebb6dd7099e4aaa2b
Iker Lizarraga <75624900+poderdelfua@users.noreply.github.com>
2024-03-14 09:55:48 +0100

    UpdateItem actualizado actividades.js

--- backend/controllers/actividades.js
+++ backend/controllers/actividades.js
@@ -1,57 +1,50 @@
-const { actividadesModel } = require("../models")
-const { handleHttpError } = require("../utils/handleError")
-const { matchedData } = require("express-validator")
+const {actividadesModel, usuarioModel} = require("../models")
+const {handleHttpError} = require("../utils/handleError")
+const {matchedData} = require("express-validator")
 
 
 const getItems = async (req, res) => {
-    try{
+    try {
         const data = await actividadesModel.find({})
-    res.send({data})
-    }catch(err){
+        res.send({data})
+    } catch (err) {
         handleHttpError(res, "ERROR_GET_USERS")
     }
 }
 
 const getItem = async (req, res) => {
-    const { id } = req.params 
+    const {id} = req.params
     try {
         const data = await actividades
         if (!data) {
             handleHttpError(res, "ACTIVIDAD_NOT_EXISTS", 404)
         }
         res.send({data})
-    }
-    catch (err) {
+    } catch (err) {
         handleHttpError(res, "ERROR_GET_ACTIVIDAD")
     }
 }
 
 const updateItem = async (req, res) => {
-    const { id } = req.params 
-    const { disponibilidad } = req.body 
-
     try {
-        const data = await actividades
-        if (!data) {
-            handleHttpError(res, "ACTIVIDAD_NOT_EXISTS", 404)
-        }
+        const {id, ...body} = matchedData(req)
+
+        const data = await usuarioModel.findByIdAndUpdate(id, body, {new: true})
         res.send({data})
-    }
-    catch (error) {
-        handleHttpError(res, "ERROR_UPDATE_ACTIVIDAD")
+    } catch (err) {
+        handleHttpError(res, "ERROR_UPDATE_USER")
     }
 }
 
 const deleteItem = async (req, res) => {
-    const { id } = req.params 
+    const {id} = req.params
     try {
         const data = await actividades
         if (!data) {
             handleHttpError(res, "ACTIVIDAD_NOT_EXISTS", 404)
         }
         res.send({data})
-    }
-    catch (error) {
+    } catch (error) {
         handleHttpError(res, "ERROR_DELETE_ACTIVIDAD")
     }
 }
@@ -60,8 +53,7 @@ const createItem = async (req, res) => {
         req = matchedData(req)
         const data = await actividadesModel.create(req)
         res.send({data})
-    }
-    catch (error) {
+    } catch (error) {
         handleHttpError(res, "ERROR_CREATE_ACTIVIDAD")
     }
 }
