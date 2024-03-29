660da0d562a080a453bf487fa1b80471e6320788
Iker Lizarraga <lizarragaiker1@gmail.com>
2024-03-30 00:25:57 +0100

    Better Buttons and notification when joined activity

--- frontend/src/components/PageCard.js
+++ frontend/src/components/PageCard.js
@@ -8,6 +8,8 @@ export default function PageCard({ page, userId, userName }) {
     const router = useRouter();
     const token = localStorage.getItem('token')
     const [userData, setUserData] = useState(null)
+    const [showNotification, setShowNotification] = useState(false);
+    const [notificationMessage, setNotificationMessage] = useState('');
 
     const handleButton = (e) => {
         //Llamada al back para añadir el usuario a la actividad
@@ -40,6 +42,17 @@ export default function PageCard({ page, userId, userName }) {
                             pageId: page._id
                         })
                     })
+                    if(!response2.ok){
+                        throw new Error('No se pudo unir al usuario a la actividad')
+                    }else{
+                        // Actualiza el estado para mostrar la notificación
+                        setNotificationMessage('Usuario unido a la actividad ✔');
+                        setShowNotification(true);
+                        setTimeout(() => {
+                            setShowNotification(false);
+                        }, 3000); // Oculta la notificación a los 3 segundos
+                    }
+
 
                 } catch (error) {
                     console.error("Error al cargar la información del usuario: ", error)
@@ -56,18 +69,25 @@ export default function PageCard({ page, userId, userName }) {
 
     return (
         <div className="flex flex-col bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
-            <img src="bg.jpg" alt="Activity" className="object-cover w-full h-32" />
+            {showNotification && (
+                <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-8 py-4 rounded-lg shadow-lg text-lg font-semibold transition-opacity duration-300 ease-in-out">
+                    {notificationMessage}
+                </div>
+            )}
+            <img src="bg.jpg" alt="Activity" className="object-cover w-full h-32"/>
             <div className="max-w-sm p-6 flex flex-col justify-between">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{page.nombre}</h5> {/* Cambiado a 'nombre' */}
                 <p className="mb-4 font-normal text-gray-700">{page.descripcion}</p> {/* Cambiado a 'descripcion' */}
                 <div className="flex items-center space-x-4">
-                    <button onClick={handeInfo} type="button" className="text-white bg-indigo-600 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    <button onClick={handeInfo} type="button"
+                            className="cursor-pointer transition-all bg-indigo-600 text-white px-6 py-2 rounded-lg border-indigo-700 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
                         Ver más información
                     </button>
                     {token !== null && (
-                        <button onClick={handleButton} type="button" className="text-white bg-indigo-600 hover:bg-indigo-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                        Unirse
-                    </button>
+                        <button onClick={handleButton} type="button"
+                                className="cursor-pointer transition-all bg-indigo-600 text-white px-6 py-2 rounded-lg border-indigo-700 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
+                            Unirse
+                        </button>
                     )}
                 </div>
             </div>
