eca37f5462bf3245852cb572b46eefae5f2c41b3
TheWhiteF <117375001+TheWhiteF@users.noreply.github.com>
2024-04-18 17:54:53 +0200

    Diseño pagina signup

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -6,6 +6,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { BACKEND_URI } from '@/config/env'
+import Image from 'next/image';
 
 //Funciona de la siguiente manera:
 //1. El usuario llena el formulario con sus datos.
@@ -85,75 +86,102 @@ export default function RegisterUser() {
     //Y al hacer clic en el botón "Registrarse", se llama a la función "handleSubmit" que envía los datos del usuario al servidor.
     return (
         <section>
-            <div className="min-h-screen bg-gray-100 flex justify-center">
-                <div className="py-6 px-8 mt-20 bg-white rounded shadow-xl">
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-6">
-                            <label htmlFor="nombre" className="block text-gray-800 font-bold">Nombre completo:</label>
-                            <input onChange={(e) => setNombre(e.target.value)} type="text" name="nombre" id="nombre" placeholder="John Doe" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
-                        </div>
+            <div className="h-screen flex justify-start">
+                <div className="absolute inset-0 z-0">
+                    <Image
+                        src="/fondo-login.png" 
+                        alt="Imagen de fondo"
+                        layout="fill"
+                        objectFit="cover" 
+                        quality={100}
+                    />
+                </div>
+            
+                <div className="w-full md:w-1/2 lg:w-2/5 z-10 p-4 md:p-8">
+                    
+                    <div className="bg-white shadow-xl rounded-lg p-6 custom-container-login">
 
-                        <div className="mb-6">
-                            <MultiSelect
-                                formFieldName={"instrumento"}
-                                options={instrumentoOptions}
-                                onChange={(selectedOptions) => setInstrumento(selectedOptions.map(option => ({ nombre: option, niveles: [] })))} 
-                                prompt="Seleccione uno o mas instrumentos" />
-                        </div>
+                    <div>
+                        <h1 htmlFor="titulo" className="text-center text-gray-3000 font-bold text-4xl">Registro</h1>
+                    </div>
 
-                        <div className="mb-6">
-                            <MultiSelect
-                                    formFieldName={"gustoMusical"}
-                                    options={generosOptions}
-                                    onChange={(selectedOptions) => setGustoMusical(selectedOptions)} 
-                                    prompt="Seleccione los géneros musicales que te interesen" />
-                        </div>
-
-                        <div className="mb-6">
-                            <label htmlFor="bio" className="block text-gray-800 font-bold">Biografía:</label>
-                            <textarea onChange={(e) => setBio(e.target.value)} name="bio" id="bio" rows="3" placeholder="Cuéntanos algo sobre ti..." className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600"></textarea>
-                        </div>
-
-                        {instrumento.map((inst, index) => (
-                            <div key={inst.nombre} className="mb-6">
-                            <MultiSelect
-                                formFieldName={`nivel_${inst.nombre}`}
-                                options={nivelOptions}
-                                value={inst.niveles}
-                                onChange={(selectedOptions) => {
-                                const newInstrumento = instrumento.map((instrumento, idx) => {
-                                    if (index === idx) {
-                                    return { ...instrumento, nivel: selectedOptions }
-                                    }
-                                    return instrumento
-                                })
-                                setInstrumento(newInstrumento)
-                                }}
-                                prompt={`Seleccione el nivel para ${inst.nombre}`}
-                            />
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-6">
+                                <label htmlFor="nombre" className="block text-gray-800 font-bold">Nombre completo:</label>
+                                <input onChange={(e) => setNombre(e.target.value)} type="text" name="nombre" id="nombre" placeholder="John Doe" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 custom-rectangulo"  />
                             </div>
-                        ))
-                        }
 
-                        
-                        <div className="mb-6">
-                            <label htmlFor="nickname" className="block text-gray-800 font-bold">Nickname:</label>
-                            <input onChange={(e) => setNickname(e.target.value)} type="text" name="nickname" id="nickname" placeholder="Tu apodo" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
-                        </div>
+                            <div className="mb-6">
+                                <MultiSelect
+                                    formFieldName={"instrumento"}
+                                    options={instrumentoOptions}
+                                    onChange={(selectedOptions) => setInstrumento(selectedOptions.map(option => ({ nombre: option, niveles: [] })))} 
+                                    prompt="Seleccione su/s Instrumento/os" />
+                            </div>
 
-                        <div className="mb-6">
-                            <label htmlFor="correo" className="block text-gray-800 font-bold">Email:</label>
-                            <input onChange={(e) => setCorreo(e.target.value)} type="email" name="correo" id="correo" placeholder="tucorreo@dominio.com" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
-                        </div>
+                            <div className="mb-6">
+                                <MultiSelect
+                                        formFieldName={"gustoMusical"}
+                                        options={generosOptions}
+                                        onChange={(selectedOptions) => setGustoMusical(selectedOptions)} 
+                                        prompt="Seleccione los géneros musicales que te interesen" />
+                            </div>
 
-                        <div className="mb-6">
-                            <label htmlFor="password" className="block text-gray-800 font-bold">Contraseña:</label>
-                            <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="contraseña" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600" />
-                        </div>
+                            <div className="mb-6">
+                                <label htmlFor="bio" className="block text-gray-800 font-bold">Biografía:</label>
+                                <textarea onChange={(e) => setBio(e.target.value)} name="bio" id="bio" rows="3" placeholder="Cuéntanos algo sobre ti..." className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 custom-rectangulo" ></textarea>
+                            </div>
 
-                        <Link href="/login" className="text-sm font-thin text-gray-800 hover:underline mt-2 mr-3 inline-block hover:text-indigo-600">¿Ya tienes una cuenta?</Link>
-                        <button type="submit" className="cursor-pointer py-2 px-4 block mt-6 bg-indigo-500 text-white font-bold w-full text-center rounded">Registrarse</button>
-                    </form>
+                            {instrumento.map((inst, index) => (
+                                <div key={inst.nombre} className="mb-6">
+                                <MultiSelect
+                                    formFieldName={`nivel_${inst.nombre}`}
+                                    options={nivelOptions}
+                                    value={inst.niveles}
+                                    onChange={(selectedOptions) => {
+                                    const newInstrumento = instrumento.map((instrumento, idx) => {
+                                        if (index === idx) {
+                                        return { ...instrumento, nivel: selectedOptions }
+                                        }
+                                        return instrumento
+                                    })
+                                    setInstrumento(newInstrumento)
+                                    }}
+                                    prompt={`Seleccione el nivel para ${inst.nombre}`}
+                                />
+                                </div>
+                            ))
+                            }
+
+                            
+                            <div className="mb-6">
+                                <label htmlFor="nickname" className="block text-gray-800 font-bold">Nickname:</label>
+                                <input onChange={(e) => setNickname(e.target.value)} type="text" name="nickname" id="nickname" placeholder="Tu apodo" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 custom-rectangulo"  />
+                            </div>
+
+                            <div className="mb-6">
+                                <label htmlFor="correo" className="block text-gray-800 font-bold">Email:</label>
+                                <input onChange={(e) => setCorreo(e.target.value)} type="email" name="correo" id="correo" placeholder="tucorreo@dominio.com" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 custom-rectangulo"  />
+                            </div>
+
+                            <div className="mb-6">
+                                <label htmlFor="password" className="block text-gray-800 font-bold">Contraseña:</label>
+                                <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="contraseña" className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 custom-rectangulo"  />
+                            </div>
+
+                            <div className="mb-4">
+                                <Link href="/login" className="text-sm font-thin-black text-gray-800 hover:underline inline-block hover:text-indigo-600">
+                                    ¿Ya tienes una cuenta?
+                                </Link>
+                            </div>
+
+                            <div className="flex justify-center mb-8">
+                                <button type="submit" className="cursor-pointer py-2 px-4 text-white font-bold w-1/2 rounded custom-cancel-button">
+                                    Registrarse
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </section>
--- frontend/src/components/MultiSelect.js
+++ frontend/src/components/MultiSelect.js
@@ -59,17 +59,17 @@ export default function MultiSelect({
     }
 
     return (
-        <label className="relative bg-white">
+        <label className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600 custom-rectangulo">
             <input type="checkbox" className="hidden peer" />
     
-            <div className="cursor-pointer after:content-['▼'] after:text-xs after:ml-1 after:inline-flex after:items-center peer-checked:after:-rotate-180 after:transition-transform inline-flex border rounded px-5 py-2">
+            <div className="cursor-pointer after:content-['▼'] after:text-xs after:ml-1 after:inline-flex after:items-center peer-checked:after:-rotate-180 after:transition-transform inline-flex rounded px-5 py-2">
                 {prompt}
                 {selectedOptions.length > 0 && (
                     <span className="ml-1 text-blue-500">{`(${selectedOptions.length} selected)`}</span>
                 )}
             </div>
     
-            <div className="absolute bg-white border transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto w-full max-h-60 overflow-y-scroll" style={{ zIndex: 100 }}>
+            <div className="absolute bg-white border transition-opacity opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto w-full max-h-60 max-w-60 overflow-y-scroll" style={{ zIndex: 100 }}>
                 {(
                     <ul>
                         <li>
