13a4781706813f4ef34a79c42fdad161187236c7
mdiezz <marcos.diez@live.u-tad.com>
2024-05-08 10:39:06 +0200

    Sidebar responisve

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,17 +1,36 @@
 "use client"
 
-import { useState } from 'react';
-import { HomeIcon, UserIcon, Cog6ToothIcon, CalendarDaysIcon } from '@heroicons/react/24/solid';
+import { useState, useEffect } from 'react';
+import { HomeIcon, UserIcon, Cog6ToothIcon, CalendarDaysIcon, Bars3Icon } from '@heroicons/react/24/solid';
 
 function Sidebar({ selectedTab = 'home' }) {
   const [activeItem, setActiveItem] = useState(selectedTab);
+  const [SidebarOpen, setSidebarOpen] = useState(false);
+
 
   const handleItemClick = (item) => {
     setActiveItem(item);
   };
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!SidebarOpen);
+  };
+
+  useEffect(() => {
+
+    //Animación automática por tamaño de pantalla
+    const handleResize = () => {
+      setSidebarOpen(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="fixed inset-y-0 left-0 w-64 bg-primary-gray rounded-r-3xl z-50">
+    <>
+    <div className={`fixed inset-y-0 bg-primary-gray rounded-r-3xl z-50 md:block transition-all duration-500 ${SidebarOpen ? 'left-0 w-64' : '-left-64 w-0'}`}>
       <div className="flex flex-col h-full">
         <div className="flex justify-center items-center pt-10 pb-2">
           <img src="/logoU.png" alt="Logo" className="h-20 w-20 rounded-full" />
@@ -116,6 +135,15 @@ function Sidebar({ selectedTab = 'home' }) {
         </nav>
       </div>
     </div>
+        
+    <button
+        onClick={toggleSidebar}
+        className={`fixed md:hidden top-5 left-5 z-50 bg-primary-gray rounded-full p-3 text-black hover:bg-light-gray`}
+    >
+        <Bars3Icon className="h-6 w-6" />
+    </button>
+    </>
+
   );
 }
 
