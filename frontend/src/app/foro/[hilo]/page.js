2bde5bcbbd1258bb417cea9ae2eba5b9a3bf0e78
Iker <lizarragaiker1@gmail.com>
2024-05-10 11:25:12 +0200

    GOAT MAN DOES IT AGAIN 2H BEFORE THE MEETING

--- frontend/src/app/foro/[hilo]/page.js
+++ frontend/src/app/foro/[hilo]/page.js
@@ -11,17 +11,17 @@ export default function HiloDetallePage() {
     const [hilo, setHilo] = useState(null);
     const [mensajes, setMensajes] = useState([]);
     const [newMessage, setNewMessage] = useState('')
-    const [like, setLike] = useState(false);
+    const [likes, setLikes] = useState({});
     const [creadoPor, setCreadoPor] = useState('');
     const [selectedTab, setSelectedTab] = useState('')
 
 
     useEffect(() => {
-      const userId = localStorage.getItem('userId');
-      if (userId) {
-          setCreadoPor(userId);
-      }
-  }, []);
+        const userId = localStorage.getItem('userId');
+        if (userId) {
+            setCreadoPor(userId);
+        }
+    }, []);
     useEffect(() => {
         if (hilo) {
             localStorage.setItem('selectedTab', hilo.titulo); // Guardar el título del hilo actual en el almacenamiento local
@@ -31,22 +31,15 @@ export default function HiloDetallePage() {
 
     useEffect(() => {
         const hiloId = localStorage.getItem('currentHiloId');
-
         const fetchHiloAndMensajes = async () => {
             try {
-
-
                 const hiloResponse = await fetch(`${BACKEND_URI}/hilo/getHiloById/${hiloId}`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
                     },
                 });
-                if (!hiloResponse.ok) {
-                    throw new Error('No se pudo cargar la información del hilo');
-                }
                 const hiloData = await hiloResponse.json();
-                console.log("Hilodata", hiloData);
                 setSelectedTab(hiloData.titulo)
                 setHilo(hiloData);
 
@@ -56,11 +49,14 @@ export default function HiloDetallePage() {
                         'Content-Type': 'application/json',
                     },
                 });
-                if (!mensajesResponse.ok) {
-                    throw new Error('No se pudo cargar la información de los mensajes');
-                }
                 const mensajesData = await mensajesResponse.json();
                 setMensajes(mensajesData);
+                // Inicializar likes para cada mensaje
+                const initialLikes = {};
+                mensajesData.forEach(msg => {
+                    initialLikes[msg._id] = msg.likes;
+                });
+                setLikes(initialLikes);
             } catch (error) {
                 console.error("Error al cargar los detalles y mensajes del hilo: ", error);
             }
@@ -70,50 +66,75 @@ export default function HiloDetallePage() {
             fetchHiloAndMensajes();
         }
     }, []);
+
     // No fuciona el crear una respuesta
     const handleSendMessage = async () => {
         const token = localStorage.getItem('token');
         const hiloId = localStorage.getItem('currentHiloId');
         if (newMessage.trim() !== '') {
             setNewMessage(newMessage.trim())
-          try {
-              const body = JSON.stringify({
-                  autorMensaje: creadoPor,
-                  mensaje: newMessage,
-                  hiloId: hiloId,
-                  likes: 0,
-                  padreMensaje: hilo._id,
-                  mediaId: "65f22f0800c7c18e6fa74aaa",
-              })
+            try {
+                const body = JSON.stringify({
+                    autorMensaje: creadoPor,
+                    mensaje: newMessage,
+                    hiloId: hiloId,
+                    likes: 0,
+                    padreMensaje: hilo._id,
+                    mediaId: "65f22f0800c7c18e6fa74aaa",
+                })
 
-            const response = await fetch(`${BACKEND_URI}/mensajes/crearPost`, {
-              method: 'POST',
-              headers: {
-                'Authorization': `Bearer ${token}`,
-                'Content-Type': 'application/json',
-              },
-                body: body,
-            })
-              console.log("Enviando mensaje...", body)
+                const response = await fetch(`${BACKEND_URI}/mensajes/crearPost`, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json',
+                    },
+                    body: body,
+                })
+                console.log("Enviando mensaje...", body)
 
-            if (!response.ok) {
-              throw new Error(response.statusText)
+                if (!response.ok) {
+                    throw new Error(response.statusText)
+                }
+                setNewMessage('')
+                window.location.reload();
+            } catch (error) {
+                console.error("Error al enviar el mensaje: ", error)
             }
-            setNewMessage('')
-              window.location.reload();
-          } catch (error) {
-            console.error("Error al enviar el mensaje: ", error)
-          }
         }
-      }
+    }
+    const handleLike = async (mensajeId) => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`${BACKEND_URI}/mensajes/likeMsg/${mensajeId}`, {
+                method: 'PATCH',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                const updatedLikes = {
+                    ...likes,
+                    [mensajeId]: likes[mensajeId] ? likes[mensajeId] + 1 : 1
+                };
+                setLikes(updatedLikes);
+            } else {
+                throw new Error('Failed to like the message');
+            }
+        } catch (error) {
+            console.error("Error al dar like al mensaje: ", error);
+        }
+    };
 
     return (
         <section className="flex flex-wrap justify-center">
             <div className="w-full md:w-1/6">
-                <SidebarForo selectedTab={selectedTab} />
+                <SidebarForo selectedTab={hilo ? hilo.titulo : ''}/>
             </div>
 
-            <div className="w-full md:w-2/3 mr-20 mb-4">
+            <div className="w-full md:w-2/3 mr-20 mb-32">
                 {hilo && (
                     <div className="relative mt-4">
                         <div className="bg-primary-gray p-8 rounded-lg shadow mb-4">
@@ -132,50 +153,62 @@ export default function HiloDetallePage() {
                                     <h3 className="text-lg mt-4 truncate">{hilo.descripcion}</h3>
                                 </div>
                             </div>
-                            
+
                         </div>
                         <div className="space-y-4">
                             {mensajes.map((mensaje) => (
-                                <div key={mensaje._id}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="gray" className="bi bi-arrow-return-right" viewBox="0 0 16 16">
-                                        <path fillRule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
+                                <div key={mensaje._id} className="relative">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="gray"
+                                         className="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd"
+                                              d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
                                     </svg>
-                                    <div className="bg-gray-50 p-4 rounded-lg shadow flex items-center ml-8">    
+                                    <div className="bg-gray-50 p-4 rounded-lg flex items-center ml-8">
                                         <div className="flex flex-col items-center" style={{flexShrink: 0}}> {}
                                             <div className="w-12 h-12 rounded-full overflow-hidden mb-2">
-                                                <img src="/no-profile.png" alt="Profile" className="w-full h-full object-cover"/>
+                                                <img src="/no-profile.png" alt="Profile"
+                                                     className="w-full h-full object-cover"/>
                                             </div>
                                             <p className="text-gray-600 text-sm">@{mensaje.autorMensaje.nickname}</p>
                                         </div>
                                         <div className="ml-4" style={{maxWidth: 'calc(100% - 5rem)', flexBasis: 'auto'}}> {}
                                             <p className="text-gray-800">{mensaje.mensaje}</p>
-                                        </div>    
+                                        </div>
+                                        {/* Mostrar el número de likes desde el estado actualizado */}
+                                        <p className="absolute bottom-0 right-0 mb-2 mt-2 mr-10">{likes[mensaje._id] || mensaje.likes}</p>
+                                        <button onClick={() => handleLike(mensaje._id)}
+                                                className="absolute bottom-0 right-0 mb-2 mt-2 mr-2 cursor-pointer">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                 fill={likes[mensaje._id] ? '#0065EF' : '#DDEBFF'}
+                                                 className="bi bi-heart-fill" viewBox="0 0 16 16">
+                                                <path fillRule="evenodd"
+                                                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                                            </svg>
+                                        </button>
                                     </div>
                                 </div>
                             ))}
-                        </div>
-                        <div className="bg-white p-4 flex w-full mb-4 mt-4">
-                            <input
-                            type="text"
-                            className="w-full border border-gray-300 rounded px-4 py-2"
-                            placeholder="Escribe un mensaje..."
-                            value={newMessage}
-                            onChange={(e) => setNewMessage(e.target.value)}
-                            />
-                            <button
-                            className="ml-5 focus:outline-none"
-                            onClick={handleSendMessage}
-                            >
-                            <PaperAirplaneIcon className="h-10 w-10 fill-gray-600 hover:fill-gray-800 cursor-pointer transition-colors duration-200" />
-                            </button>
+                            <div className="bg-white p-4 flex fixed bottom-4 md:w-2/3 mb-4 justify-center">
+                                <input
+                                    type="text"
+                                    className="w-full border border-gray-300 rounded px-4 py-2"
+                                    placeholder="Escribe un mensaje..."
+                                    value={newMessage}
+                                    onChange={(e) => setNewMessage(e.target.value)}
+                                />
+                                <button className="ml-5 focus:outline-none" onClick={handleSendMessage}>
+                                    <PaperAirplaneIcon
+                                        className="h-10 w-10 fill-gray-600 hover:fill-gray-800 cursor-pointer transition-colors duration-200"/>
+                                </button>
+                            </div>
                         </div>
                     </div>
 
                 )}
             </div>
-
             <div className="fixed top-0 right-0 m-4">
-                <a href="/foro" className={"bg-primary-gray w-10 h-10 flex items-center justify-center rounded-full border-gray-400 border-b-[2px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[2px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] ml-auto"}>
+                <a href="/foro"
+                   className={"bg-primary-gray w-10 h-10 flex items-center justify-center rounded-full border-gray-400 border-b-[2px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[2px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px] ml-auto"}>
                     <strong>X</strong>
                 </a>
             </div>
