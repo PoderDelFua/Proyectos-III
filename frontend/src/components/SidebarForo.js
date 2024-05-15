792cb99891d3bc920c1ad7749860049211724a97
Rubén <rubypascu@hotmail.com>
2024-05-15 14:39:26 +0200

    Modificaciones SidebarForo(quedan bordes redondos)

--- frontend/src/components/SidebarForo.js
+++ frontend/src/components/SidebarForo.js
@@ -1,22 +1,25 @@
+"use client"
+
 import { useEffect, useState } from 'react';
 import { BACKEND_URI } from "@/config/env";
 import { useRouter } from 'next/navigation';
+import { Bars3Icon } from '@heroicons/react/24/solid';
 
 function SidebarForo({ selectedTab = '' }) {
     const [activeItem, setActiveItem] = useState(selectedTab.toLowerCase());
     const [hilos, setHilos] = useState([]);
+    const [SidebarOpen, setSidebarOpen] = useState(false);
     const router = useRouter();
+
     useEffect(() => {
-        const selectedTabLocal = localStorage.getItem('selectedTab'); // Obtener el título del hilo seleccionado desde el almacenamiento local
+        const selectedTabLocal = localStorage.getItem('selectedTab');
         if (selectedTabLocal) {
-            setActiveItem(selectedTabLocal.toLowerCase()); // Establecer el hilo activo basado en el título almacenado
+            setActiveItem(selectedTabLocal.toLowerCase());
         }
     }, [selectedTab]);
 
-
     useEffect(() => {
         const fetchData = async () => {
-
             try {
                 const response = await fetch(`${BACKEND_URI}/hilo/hilosPublicos`, {
                     method: 'GET',
@@ -45,40 +48,78 @@ function SidebarForo({ selectedTab = '' }) {
     const handleItemClick = (hilo) => {
         setActiveItem(hilo.titulo.toLowerCase());
         localStorage.setItem('currentHiloId', hilo._id);
-        localStorage.setItem('selectedTab', hilo.titulo); // Guardar el título del nuevo hilo seleccionado
-        router.push(`/foro/${hilo._id}`); // Navegar a la página del hilo seleccionado
-        window.location.reload(); // Opcional: Recargar la página para refrescar el estado completamente
+        localStorage.setItem('selectedTab', hilo.titulo);
+        router.push(`/foro/${hilo._id}`);
+        window.location.reload();
     };
 
+    const toggleSidebar = () => {
+        setSidebarOpen(!SidebarOpen);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSidebarOpen(window.innerWidth >= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
-        <div className="fixed inset-y-0 left-0 w-64 rounded-r-3xl z-50 overflow-hidden" style={{ backgroundImage: "url('/fondo-sidebar.png')" }}>
-            <div className="flex flex-col h-full">
-                <nav className="flex-1 pl-4 py-4 space-y-4 overflow-y-auto">
-                    <h2 className="text-2xl font-bold text-white">Foro</h2>
-                    <h2 className="text-xl font-bold text-white">Temas</h2>
-                    {hilos.map((hilo) => (
-                        <div
-                            key={hilo.titulo}
-                            className={`group flex items-center py-2 px-4 text-black ${activeItem === hilo.titulo.toLowerCase() ? 'bg-white rounded-l-full' : 'hover:bg-light-gray rounded-full'}`}
-                            onClick={() => handleItemClick(hilo)}
-                        >
-                            <div className="flex justify-between items-center p-2 w-full">
-                                <div>
-                                    <p className="font-medium">{hilo.titulo}</p>
-                                    <p className="text-sm text-gray-600">Creador: {hilo.creador}</p>
-                                    <p className="text-sm text-gray-600">{hilo.postCount} posts</p>
-                                </div>
-                                {activeItem === hilo.titulo.toLowerCase() && (
-                                    <div className="absolute inset-y-0 right-0 flex items-center pr-4">
+        <>
+            {SidebarOpen && window.innerWidth < 768 && (
+                <div className={`fixed inset-0 bg-black bg-opacity-50 z-40`} onClick={toggleSidebar}></div>
+            )}
+
+            <div className={`fixed inset-y-0 bg-primary-gray rounded-r-3xl z-50 md:block transition-all duration-500 ${SidebarOpen ? 'left-0 w-64' : '-left-64 w-0'}`} style={{ backgroundImage: "url('/fondo-sidebar.png')" }}>
+                <div className="flex flex-col h-full">
+                    <nav className="flex-1 pl-4 py-4 space-y-4 overflow-y-auto">
+                        <button className="flex items-center text-white  hover:font-bold  icon-custom" onClick={() => router.push("/home")}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+                            </svg>
+                            <h2 className="text-2xl ml-2">Foro</h2>
+                        </button>
+                        <button className="flex items-center text-white" onClick={() => router.push("/foro")}>    
+                            <h2 className="text-xl hover:font-bold text-white ml-10">Temas</h2>
+                        </button>
+                        
+                        {hilos.map((hilo) => (
+                            <div
+                                key={hilo.titulo}
+                                className={`group flex items-center py-2 px-4 text-black transition-all duration-300 ${activeItem === hilo.titulo.toLowerCase() ? 'bg-white rounded-l-full' : 'text-white hover:bg-light-gray hover:text-blue rounded-full'}`}
+                                onClick={() => handleItemClick(hilo)}
+                            >
+                                <div className="flex justify-between items-center p-2 w-full">
+                                    <div>
+                                        <p className={`text-sm ${activeItem == hilo.titulo.toLowerCase() ? 'text-custom-active' : 'text-custom-secundario'}`}>Creador: {hilo.creador}</p>
+                                        <p className="font-medium overflow-hidden whitespace-nowrap text-ellipsis" style={{ maxWidth: "calc(100% - 0.8rem)" }}>
+                                            {hilo.titulo.length > 26 ? `${hilo.titulo.substring(0, 26)}...` : hilo.titulo}
+                                        </p>
+                                        <p className={`text-sm ${activeItem == hilo.titulo.toLowerCase() ? 'text-custom-active' : 'text-custom-secundario'}`}>{hilo.postCount} posts</p>
                                     </div>
-                                )}
+                                    {activeItem === hilo.titulo.toLowerCase() && (
+                                        <div className="relative">
+                                            <div className="absolute top-0 right-0 h-5 w-5 bg-white"></div>
+                                            <div className="absolute top-0 right-0 h-5 w-5 bg-sidebar-light-blue rounded-tr-full"></div>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </nav>
+                        ))}
+                    </nav>
+                </div>
             </div>
-        </div>
+
+            <button
+                onClick={toggleSidebar}
+                className={`fixed md:hidden top-5 left-5 z-50 bg-primary-gray rounded-full p-3 text-black hover:bg-light-gray`}
+            >
+                <Bars3Icon className="h-6 w-6" />
+            </button>
+        </>
     );
 }
 
