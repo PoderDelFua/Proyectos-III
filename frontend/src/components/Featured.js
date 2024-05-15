70f1f30e81eb2b34787e9a960c0f4b22eeda110f
Rubén <rubypascu@hotmail.com>
2024-05-16 00:55:51 +0200

    Navbar de foro en el home

--- frontend/src/components/Featured.js
+++ frontend/src/components/Featured.js
@@ -1,33 +1,59 @@
 "use client"
 
-import { useState } from 'react';
+import { BACKEND_URI } from "@/config/env";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 function Featured() {
-  // Sample data for forums
-  const forums = [
-    { name: 'Foro1', creator: '@nickname1', postCount: 50 },
-    { name: 'Foro2', creator: '@nickname2', postCount: 26 },
-    { name: 'Foro3', creator: '@nickname3', postCount: 38 },
-    // ... add more forums as needed
-  ];
+  const [hilos, setHilos] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchData = async () => {
+          try {
+              const response = await fetch(`${BACKEND_URI}/hilo/hilosPublicos`, {
+                  method: 'GET',
+                  headers: {
+                      'Content-Type': 'application/json',
+                  },
+              });
+              if (!response.ok) {
+                  throw new Error('No se pudo cargar la información de los hilos');
+              }
+              const data = await response.json();
+              setHilos(data.map(hilo => ({
+                  _id: hilo._id,
+                  titulo: hilo.titulo,
+                  creador: `@${hilo.creadorId.nickname}`,
+                  postCount: hilo.postCount
+              })));
+          } catch (error) {
+              console.error("Error al cargar la información de los hilos: ", error);
+          }
+      }
+
+      fetchData();
+  });
 
   return (
-    <div className="fixed inset-y-0 right-0 w-64 bg-white shadow-2xl rounded-l-3xl p-4">
+    <div className="fixed inset-y-0 right-0 w-64 shadow-2xl rounded-l-3xl p-4 custom-bg-color2">
       <div className="flex justify-between items-center">
-        <h2 className="text-lg font-semibold">Destacados</h2>
+        <h2 className="text-2xl ml-2 font-bold">Foro</h2>
       </div>
-      <div className="mt-4">
-        {forums.map((forum, index) => (
-          <div key={index} className="flex justify-between items-center p-2 hover:bg-gray-100 rounded-md">
-            <div>
-              <p className="font-medium">{forum.name}</p>
-              <p className="text-sm text-gray-600">Creador: {forum.creator}</p>
-              <p className="text-sm text-gray-600">{forum.postCount} posts</p>
+      <nav className="flex-1 py-6">
+        {hilos.map((hilo) => (    
+          <a key={hilo._id} href={`/foro/${hilo._id}`} className="block hover:bg-white rounded-md rounded-[1.5rem] p-2">
+            <div className="flex flex-col pl-5 py-4 justify-between w-full">
+              <p className="text-sm text-custom-active">Creador: {hilo.creador}</p>
+              <p className="font-medium overflow-hidden whitespace-nowrap text-ellipsis" style={{ maxWidth: "calc(100% - 0.8rem)" }}>
+                {hilo.titulo.length > 26 ? `${hilo.titulo.substring(0, 26)}...` : hilo.titulo}
+              </p>
+              <p className="text-sm text-custom-active">{hilo.postCount} posts</p>
             </div>
-          </div>
+          </a> 
         ))}
-      </div>
-      <div className="text-center mt-4">
+      </nav>
+      <div className="text-center mt-2">
         <a href="/foro" className="text-sm text-blue-600 hover:underline">Mostrar más</a>
       </div>
     </div>
